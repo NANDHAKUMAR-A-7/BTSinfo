5d93d6ef1206d61a9b4e8466f09eeea63dcb9383
Nandhakumar A <fdemo1177@gmail.com>
2025-06-25 14:41:49 +0530

    added code prettier

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 
 // Wait 3 seconds then fade out and redirect
 setTimeout(() => {
-const popup = document.getElementById("popup");
-popup.classList.add("fade-out");
+  const popup = document.getElementById("popup");
+  popup.classList.add("fade-out");
 
-// Optional: add small delay before redirect to allow fade
-setTimeout(() => {
-window.location.href = "index1.html"; // Change to your main page
-}, 500);
+  // Optional: add small delay before redirect to allow fade
+  setTimeout(() => {
+    window.location.href = "index1.html"; // Change to your main page
+  }, 500);
 }, 2000);
 
 // end of prog time delay.
-
--- sw.js
+++ sw.js
@@ -1,40 +1,34 @@
 //add service worker connection
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open('pwa-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-      
-        '/manifest.json',
-        '/icon.png'
-      ]);
+    caches.open("pwa-cache").then(function (cache) {
+      return cache.addAll(["/", "/manifest.json", "/icon.png"]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
 
-// add event listener for send button 
+// add event listener for send button
 
 const sendbtn = document.getElementById("sendbtn");
 const username = document.getElementById("name");
 const mail = document.getElementById("email");
 const message = document.getElementById("message");
 
-    message.addEventListener("click",function (event)
-{
-    console.log(event.target);
-    sendbtn.style.display = "block";
+message.addEventListener("click", function (event) {
+  console.log(event.target);
+  sendbtn.style.display = "block";
 });
-    sendbtn.addEventListener("click" ,function(event){
-        alert("Get Replay As Soon As Once Our Team Receive Your Message ! ...Thank You For Contacting Us."           
-        )
-    });
+sendbtn.addEventListener("click", function (event) {
+  alert(
+    "Get Replay As Soon As Once Our Team Receive Your Message ! ...Thank You For Contacting Us."
+  );
+});
