ef590957884b6e0e51b6349f8da7633a2f826c77
Nandhakumar A <fdemo1177@gmail.com>
2025-06-27 22:35:23 +0530

    new changes in site

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+window.onload = function () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.1 // activates when at least 10% of the element is visible
+  });
+
+   const target = document.querySelector('#ourservices');
+  if (target) {
+    observer.observe(target);
+  }
+};
 
 const contactmain = document.getElementById("via-mobile");
 const content1 = document.getElementById("whatsapp-contact");
 
-function display(){
-   contactmain.addEventListener("click",function() {
-   content1.style.display = "block";
-   contactmain.style.display = "none";
-   });
+function display() {
+  contactmain.addEventListener("click", function () {
+    content1.style.display = "block";
+    contactmain.style.display = "none";
+  });
 }
-console.log((display()));
+console.log(display());
 
 console.log("Main JS loaded.");
 
